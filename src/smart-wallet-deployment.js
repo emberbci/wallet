10ce872b8c95f43b00b493f59e15ecffae4e4d3c
akashbiswas0 <akash8860biswas@gmail.com>
2026-03-23 10:30:00 +0530

    feat(ext): add lit-cosigned send flow for smart wallets

---
+++ src/smart-wallet-deployment.js
@@ -0,0 +1,7 @@
+export const SMART_WALLET_DEPLOYMENT = {
+  entryPointAddress: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+  legacyFactoryAddress: "0x97676DB493423EEe28c23427650E49ff1F80366E",
+  legacyFactoryDeploymentBlock: 10565300n,
+  factoryAddress: "0x9191A2C949F3ec122B4E774202271A62d1161130",
+  factoryDeploymentBlock: 10565783n,
+};
